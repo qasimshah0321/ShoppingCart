4f0e1a827e80af19d545464c97ffcb90e4f1531d
Muhammad Qasim <qasimshah33@hotmail.com>
2021-05-28 10:07:10 +0500

    commited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabase,
+    AngularFireAuth
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBMSwsnYfJpb0U2N-R3BPHJDx1kmVJamvk",
+    authDomain: "oshop-4e5f0.firebaseapp.com",
+    projectId: "oshop-4e5f0",
+    storageBucket: "oshop-4e5f0.appspot.com",
+    messagingSenderId: "1027798963009",
+    appId: "1:1027798963009:web:deaa98b1003183168b47c0",
+    measurementId: "G-W3LXZY8HVX"
+  }
 };
 
 /*
